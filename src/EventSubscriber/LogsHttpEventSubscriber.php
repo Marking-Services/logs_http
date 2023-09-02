07d01bc2c0a5c7b4b9a70244858f7fb020164ac0
Alan <alan@atendesigngroup.com>
2023-09-02 19:23:58 +0200

    Drupal 10 compatibility for RequestEvent and info.yml

--- src/EventSubscriber/LogsHttpEventSubscriber.php
+++ src/EventSubscriber/LogsHttpEventSubscriber.php
@@ -3,8 +3,8 @@
 namespace Drupal\logs_http\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 /**
  * Event subscribed for Logs http.
@@ -14,10 +14,10 @@ class LogsHttpEventSubscriber implements EventSubscriberInterface {
   /**
    * Initializes Logs http module requirements.
    *
-   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
    *   The event to process.
    */
-  public function onRequest(GetResponseEvent $event) {
+  public function onRequest(RequestEvent $event) {
     drupal_register_shutdown_function('logs_http_shutdown');
   }
 
